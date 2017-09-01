eb3a7dd91f58882498ebbf437b531212f5c65ae7
Fernando Moreira <nandomoreira.me@gmail.com>
2017-08-31 21:14:00 -0300

    added vue-styleguidist for generate styleguide

--- src/components/Autocomplete.vue
+++ src/components/Autocomplete.vue
@@ -24,74 +24,134 @@
 </style>
 
 <script>
-  export default {
-    data () {
-      return {
-        open: false,
-        current: 0
+/**
+ * Component is described here.
+ *
+ * @example ./extra.examples.md
+ */
+export default {
+  name: 'Autocomplete',
+  data () {
+    return {
+      /**
+      * The color for the button.
+      */
+      open: false,
+      /**
+      * The color for the button.
+      */
+      current: 0
+    }
+  },
+  props: {
+    /**
+     * Gets called when the user clicks on the button
+     *
+     * @param {SyntheticEvent} event The react `SyntheticEvent`
+     * @param {Number} num Numbers of examples
+     */
+    value: {
+      type: String,
+      required: true
+    },
+    /**
+     * Gets called when the user clicks on the button
+     *
+     * @param {SyntheticEvent} event The react `SyntheticEvent`
+     * @param {Number} num Numbers of examples
+     */
+    suggestions: {
+      type: Array,
+      required: true
+    }
+  },
+  computed: {
+    /**
+     * Gets called when the user clicks on the button
+     *
+     * @param {SyntheticEvent} event The react `SyntheticEvent`
+     * @param {Number} num Numbers of examples
+     */
+    matches () {
+      return this.suggestions.filter(obj => obj.city.indexOf(this.value) >= 0)
+    },
+    /**
+     * Gets called when the user clicks on the button
+     *
+     * @param {SyntheticEvent} event The react `SyntheticEvent`
+     * @param {Number} num Numbers of examples
+     */
+    openSuggestion () {
+      return this.selection !== '' &&
+            this.matches.length !== 0 &&
+            this.open === true
+    }
+  },
+  methods: {
+    /**
+     * Gets called when the user clicks on the button
+     *
+     * @param {SyntheticEvent} event The react `SyntheticEvent`
+     * @param {Number} num Numbers of examples
+     */
+    updateValue (value) {
+      if (this.open === false) {
+        this.open = true
+        this.current = 0
+      }
+      this.$emit('input', value)
+    },
+    /**
+     * Gets called when the user clicks on the button
+     *
+     * @param {SyntheticEvent} event The react `SyntheticEvent`
+     * @param {Number} num Numbers of examples
+     */
+    enter () {
+      this.$emit('input', this.matches[this.current].city)
+      this.open = false
+    },
+    /**
+     * Gets called when the user clicks on the button
+     *
+     * @param {SyntheticEvent} event The react `SyntheticEvent`
+     * @param {Number} num Numbers of examples
+     */
+    up () {
+      if (this.current > 0) {
+        this.current--
       }
     },
-    props: {
-      value: {
-        type: String,
-        required: true
-      },
-      suggestions: {
-        type: Array,
-        required: true
+    /**
+     * Gets called when the user clicks on the button
+     *
+     * @param {SyntheticEvent} event The react `SyntheticEvent`
+     * @param {Number} num Numbers of examples
+     */
+    down () {
+      if (this.current < this.matches.length - 1) {
+        this.current++
       }
     },
-    computed: {
-      matches () {
-        return this.suggestions.filter(obj => obj.city.indexOf(this.value) >= 0)
-      },
-      openSuggestion () {
-        return this.selection !== '' &&
-              this.matches.length !== 0 &&
-              this.open === true
-      }
+    /**
+     * Gets called when the user clicks on the button
+     *
+     * @param {SyntheticEvent} event The react `SyntheticEvent`
+     * @param {Number} num Numbers of examples
+     */
+    isActive (index) {
+      return index === this.current
     },
-    methods: {
-      // Triggered the input event to cascade the updates to
-      // parent component
-      updateValue (value) {
-        if (this.open === false) {
-          this.open = true
-          this.current = 0
-        }
-        this.$emit('input', value)
-      },
-
-      // When enter key pressed on the input
-      enter () {
-        this.$emit('input', this.matches[this.current].city)
-        this.open = false
-      },
-
-      // When up arrow pressed while suggestions are open
-      up () {
-        if (this.current > 0) {
-          this.current--
-        }
-      },
-
-      // When down arrow pressed while suggestions are open
-      down () {
-        if (this.current < this.matches.length - 1) {
-          this.current++
-        }
-      },
-
-      // For highlighting element
-      isActive (index) {
-        return index === this.current
-      },
-
-      // When one of the suggestion is clicked
-      suggestionClick (index) {
-        this.$emit('input', this.matches[index].city)
-        this.open = false
-      }
+    /**
+     * Gets called when the user clicks on the button
+     *
+     * @param {SyntheticEvent} event The react `SyntheticEvent`
+     * @param {Number} num Numbers of examples
+     */
+    suggestionClick (index) {
+      this.$emit('input', this.matches[index].city)
+      this.open = false
     }
   }
+}
 </script>
---
+++ styleguide.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  sections: [
+    {
+      name: 'Introduction',
+      content: 'docs/introduction.md'
+    },
+    {
+      name: 'Documentation',
+      sections: [
+        {
+          name: 'Installation',
+          content: 'docs/installation.md'
+        },
+        {
+          name: 'Configuration',
+          content: 'docs/configuration.md'
+        }
+      ]
+    },
+    {
+      name: 'UI Components',
+      content: 'docs/ui.md',
+      components: 'lib/components/ui/*.vue'
+    }
+  ]
+}
